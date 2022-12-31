5b87e557b0e52d27ce37c317da47bb4fb5268c48
M5c8n31d3r <m.schneider-kleintransporte@web.de>
2022-12-31 21:53:58 +0100

    finalQuizApp

--- script.js
+++ script.js
@@ -44,26 +44,117 @@ let questions = [
 
 
 let currentQuestion = 0;
-
-
+let rightQuestions = 0;
+let AUDIO_SUCCESS = new Audio('audio/success.mp3');
+let AUDIO_FAIL = new Audio('audio/fail.mp3');
 
 function init(){
     document.getElementById('all-questions').innerHTML = questions.length;
 
     showQuestion();
+    
+}
 
+function showQuestion(){
+
+    if (currentQuestion >= questions.length) {
+        showEndScreen();
+    } else {  // Show question
+        updateToNextQuestion();
+
+    }
+}
+
+function showEndScreen(){
+     
+            document.getElementById('endScreen').style = '';
+            document.getElementById('questionBody').style = 'display: none';
+    
+            document.getElementById('amount-of-questions').innerHTML = questions.length;
+            document.getElementById('amount-of-right-questions').innerHTML = rightQuestions;
+            document.getElementById('header-image').src = 'img/cup.jpg';
 }
 
 
-function showQuestion(){
+function updateToNextQuestion() {
+    let percent = (currentQuestion +1) / questions.length;
+    percent = Math.round(percent * 100);
+    document.getElementById('progress-bar').innerHTML = `${percent}%`;
+    document.getElementById('progress-bar').style = `width: ${percent}%`;
+
+
     let question = questions[currentQuestion];
     
+    document.getElementById('question-number').innerHTML = currentQuestion +1;
     document.getElementById('questiontext').innerHTML = question['question'];
     document.getElementById('answer_1').innerHTML = question['answer_1'];
     document.getElementById('answer_2').innerHTML = question['answer_2'];
     document.getElementById('answer_3').innerHTML = question['answer_3'];
     document.getElementById('answer_4').innerHTML = question['answer_4'];
-
-
-
 }
+
+function answer(selection){
+    let question = questions[currentQuestion];                     
+    let selectedQuestionNumber = selection.slice(-1);                 
+    let idOfRightAnswer = `answer_${question['right_answer']}`;    
+        
+    if(selectedQuestionNumber == question['right_answer']) {  // Richige Frage beantwortet
+        document.getElementById(selection).parentNode.classList.add('bg-success');           
+        AUDIO_SUCCESS.play();
+        rightQuestions++;
+    } else {
+        document.getElementById(selection).parentNode.classList.add('bg-danger');            
+        document.getElementById(idOfRightAnswer).parentNode.classList.add('bg-success');
+        AUDIO_FAIL.play();
+    }
+        document.getElementById('next-button').disabled = false;
+}
+
+function nextQuestion(){
+    currentQuestion++;   // z.B. von 0 auf 1
+    document.getElementById('next-button').disabled = true;  // button wird gesperrt
+    resetAnserButtons();
+    showQuestion();
+}
+
+function resetAnserButtons() {
+    document.getElementById('answer_1').parentNode.classList.remove('bg-danger');
+    document.getElementById('answer_1').parentNode.classList.remove('bg-success');
+    document.getElementById('answer_2').parentNode.classList.remove('bg-danger');
+    document.getElementById('answer_2').parentNode.classList.remove('bg-success');
+    document.getElementById('answer_3').parentNode.classList.remove('bg-danger');
+    document.getElementById('answer_3').parentNode.classList.remove('bg-success');
+    document.getElementById('answer_4').parentNode.classList.remove('bg-danger');
+    document.getElementById('answer_4').parentNode.classList.remove('bg-success');
+}
+
+function restartGame() {
+    document.getElementById('header-image').src = 'img/tree.jpg';
+    document.getElementById('questionBody').style = '';  
+    document.getElementById('endScreen').style = 'display: none';  
+    currentQuestion = 0;
+    rightQuestions = 0;
+    init();
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
